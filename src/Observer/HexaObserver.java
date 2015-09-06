c71572c0e46b6947cc1b58c5db302e2a3da184a4
邓慧 <783352994@qq.com>
2015-09-06 18:50:46 +0800

    加入观察者模式

---
+++ src/Observer/HexaObserver.java
@@ -0,0 +1,17 @@
+package Observer;
+
+/**
+ * Created by zero on 15-9-6.
+ */
+public class HexaObserver extends Observer{
+    public HexaObserver(Subject subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println( "Hex String: "
+                + Integer.toHexString( subject.getState() ).toUpperCase() );
+    }
+}
---
+++ src/Observer/Observer.java
@@ -0,0 +1,9 @@
+package Observer;
+
+/**
+ * Created by zero on 15-9-6.
+ */
+public abstract class Observer {
+    protected Subject subject;
+    public abstract void update();
+}
